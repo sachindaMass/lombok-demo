bfe7e4a82493738f573318e7437273a33f8a8d01
sachindaMass <sachinda46@gmail.com>
2023-02-16 21:15:17 +0530

    Setting up Lombok for Maven Project

---
+++ src/main/java/Product.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+public class Product {
+
+    @NonNull
+    private String name;
+    private String description;
+    private String type;
+    private String category;
+    private Double price;
+}
